f1c8144d2e70dc1ddbd6f35a1dc9d802c32ddeda
Elek Mate <elekamate@gmail.com>
2019-05-29 04:34:54 +0200

    Chore(Tamagotchi): WelcomeMessage added.

---
+++ Tamagotchi/Tamagotchi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi
+{
+    class Tamagotchi
+    {
+        public static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome!");
+            Console.WriteLine("To create a tamagotchi press ''c''");
+        }
+        public static void CreateAnimal()
+        {
+
+        }
+    }
+}
